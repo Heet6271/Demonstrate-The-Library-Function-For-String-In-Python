9c2e5a17b74c00d387d210b62bdeebcaca7aed38
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 11:05:05 +0530

    p21

---
+++ Prac21.py
@@ -0,0 +1,18 @@
+h="hello"
+z="LOWER"
+e="hello how are you"
+x="     hello"
+c="helloall"
+print(h.capitalize())
+print(h.upper())
+print(z.lower())
+print(z.casefold())
+print(z.count('L'))
+g=h.replace('h' , 'k')
+print(g)
+print(h.islower())
+l=e.split()
+print(l)
+print(x.lstrip())
+print(c.rstrip())
+print(c.center(20,'*'))
